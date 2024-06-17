015adc300abf9c1deec47da712bc0d3e95002002
Valentin Nasraty <valentin.nasraty@phantombuster.com>
2024-06-17 23:48:57 +0200

    add two candidates, images and bio

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Navbar() {
     return (
         <nav>
-            <div className="flex p-12 justify-between bg-my-orange text-white">
+            <div className="flex py-12 px-24 justify-between bg-my-orange text-white">
                 <div className="flex">
                     <h1 className="font-[1000] text-4xl">Faisons vivre Marseille !</h1>
                 </div>
