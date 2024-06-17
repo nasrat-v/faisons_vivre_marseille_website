cbdb06a38782c62bc816fcfc2246bf11efb9527b
Valentin Nasraty <valentin.nasraty@phantombuster.com>
2024-06-17 22:04:23 +0200

    add navbar with font - cannot load custom font wip

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./navbar";
 import Footer from "./Footer";
 import Introduction from "./Introduction";
 import Candidates from "./Candidates";
 import Propositions from "./Propositions";
 import Contacts from "./Contacts";
+import Navbar from "./Navbar";
 
 export default function LayoutLayout() {
     return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,16 @@ import React from "react";
 export default function Navbar() {
     return (
         <nav>
-            <h1>Faisons vivre Marseille !</h1>
-            <Link to="/">Les candidats</Link>
-            <Link to="/">Nos propositions</Link>
-            <Link to="/">Nous suivre</Link>
+            <div className="flex p-12 justify-between bg-my-orange text-white">
+                <div className="flex">
+                    <h1 className="font-[1000] text-4xl">Faisons vivre Marseille !</h1>
+                </div>
+                <div className="flex justify-evenly gap-12 items-end font-bold italic text-2xl">
+                    <Link to="/">Les candidats</Link>
+                    <Link to="/">Nos propositions</Link>
+                    <Link to="/">Nous suivre</Link>
+                </div>
+            </div>
         </nav>
     )
 }
