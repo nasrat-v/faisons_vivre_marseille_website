7d7f327512811383015096c38b7154c0bf3c986d
Valentin Nasraty <valentin.nasraty@phantombuster.com>
2024-06-18 07:09:08 +0200

    add custom font futura

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         <nav>
             <div className="flex py-12 px-24 justify-between bg-my-orange text-white">
                 <div className="flex">
-                    <h1 className="font-[1000] text-4xl">Faisons vivre Marseille !</h1>
+                    <h1 className="font-futura-bold text-4xl">Faisons vivre Marseille !</h1>
                 </div>
                 <div className="flex justify-evenly gap-12 items-end font-bold italic text-2xl">
                     <Link to="/">Les candidats</Link>
