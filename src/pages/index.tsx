93a9b9abbcd7eba4949e45fe843ae9e0d7dd3bcd
Valentin Nasraty <valentin.nasraty@phantombuster.com>
2024-06-18 07:57:22 +0200

    use woff2 + add meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <section>
+      <meta http-equiv="Content-Type" charSet="utf-8" content="text/html; charset=UTF-8" /> 
       <Layout />
     </section>
   )
