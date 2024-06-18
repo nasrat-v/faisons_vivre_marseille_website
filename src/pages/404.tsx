da4c5f678ce2c633cd989a647c2ca860e08b626a
Valentin Nasraty <valentin.nasraty@phantombuster.com>
2024-06-18 11:19:05 +0200

    add not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,17 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const NotFoundPage: React.FC<PageProps> = () => {
+export default function NotFound() {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <div className="flex-column">
+      <Navbar noLinks={true} />
+      <div className="flex md:px-24 px-12 text-center h-screen items-center justify-center">
+        <span className="text-my-orange text-4xl">
+          Page introuvable
+        </span>
+      </div>
+      <Footer />
+    </div>
   )
 }
-
-export default NotFoundPage
-
-export const Head: HeadFC = () => <title>Not found</title>
